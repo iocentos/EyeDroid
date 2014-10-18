0cf1f973bd61f85f50c4dc1353470a90d797fe2a
Ioannis Sintos <sintosioannis@outlook.com>
2014-10-18 22:45:49 +0200

    Finished camera protocol

--- EyeDroid/src/dk/itu/eyedroid/io/InputStreamCamera.java
+++ EyeDroid/src/dk/itu/eyedroid/io/InputStreamCamera.java
@@ -11,10 +11,12 @@ import org.opencv.android.JavaCameraView;
 import org.opencv.android.LoaderCallbackInterface;
 import org.opencv.android.NativeCameraView;
 import org.opencv.android.OpenCVLoader;
+import org.opencv.android.Utils;
 import org.opencv.core.Mat;
 
 import dk.itu.eyedroid.R;
 import android.content.Context;
+import android.graphics.Bitmap;
 import android.graphics.Camera;
 import android.util.Log;
 import dk.itu.eyedroid.MainActivity;
@@ -23,22 +25,29 @@ import dk.itu.spcl.jlpf.io.IOProtocolReader;
 
 public class InputStreamCamera implements IOProtocolReader,
 		CvCameraViewListener2 {
-	
+
 	private static final String TAG = "InputStreamCamera";
 
 	private CameraBridgeViewBase mOpenCvCameraView;
 	private Context mContext;
-	
+
 	private int mCameraId;
 
 	private Mat rgba;
 	private Mat grey;
-	
 
-	public InputStreamCamera(Context context , CameraBridgeViewBase camera , int camId ){
+	private CountDownLatch startGate;
+	private CountDownLatch endGate;
+
+	private Bitmap mBitmap;
+
+	public InputStreamCamera(Context context, CameraBridgeViewBase camera,
+			int camId) {
 		mContext = context;
 		mOpenCvCameraView = camera;
 		mCameraId = camId;
+		startGate = new CountDownLatch(1);
+		endGate = new CountDownLatch(1);
 
 	}
 
@@ -50,12 +59,11 @@ public class InputStreamCamera implements IOProtocolReader,
 
 	@Override
 	public void init() {
-		
-		
+
 		mOpenCvCameraView.setCameraIndex(mCameraId);
-		
+
 		mOpenCvCameraView.setCvCameraViewListener(this);
-		
+
 		BaseLoaderCallback mLoaderCallbacks = new BaseLoaderCallback(mContext) {
 
 			@Override
@@ -74,14 +82,27 @@ public class InputStreamCamera implements IOProtocolReader,
 				mOpenCvCameraView.enableView();
 			}
 		};
-		
-		OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_2_4_9, mContext, mLoaderCallbacks);
+
+		OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_2_4_9, mContext,
+				mLoaderCallbacks);
 	}
 
 	@Override
 	public Bundle read() throws IOException {
-		//TODO implement with latch
-		return null;
+
+		Bundle bundle = new Bundle();
+		try {
+			startGate.await();
+			Log.i(TAG, "reader entered read method");
+			bundle.put(InputNetStreamingProtocol.INPUT_BITMAP, mBitmap);
+			startGate = new CountDownLatch(1);
+			endGate.countDown();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+			throw new IOException();
+		}
+
+		return bundle;
 	}
 
 	@Override
@@ -102,8 +123,26 @@ public class InputStreamCamera implements IOProtocolReader,
 	@Override
 	public Mat onCameraFrame(CvCameraViewFrame inputFrame) {
 		Log.i(TAG, "got frame");
-		Log.i(TAG , "frame " + inputFrame.rgba().width()  + "   " + 
-				inputFrame.rgba().height());
+
+		rgba = inputFrame.rgba();
+
+		try {
+			mBitmap = Bitmap.createBitmap(rgba.cols(), rgba.rows(),
+					Bitmap.Config.ARGB_8888);
+			Utils.matToBitmap(rgba, mBitmap);
+		} catch (Exception ex) {
+			System.out.println(ex.getMessage());
+		}
+
+		startGate.countDown();
+		try {
+			endGate.await();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		endGate = new CountDownLatch(1);
+
 		return null;
 	}
 
