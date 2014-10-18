b4a9614d6d55ff8ce444b7368cc23b6851e77c5b
danielgg88 <daniel.gg88@gmail.com>
2014-10-18 22:42:18 +0200

    OutputNetTCPProtocol added. Contains a bug when clients reconnect.

--- EyeDroid/src/dk/itu/eyedroid/TestFragment.java
+++ EyeDroid/src/dk/itu/eyedroid/TestFragment.java
@@ -3,12 +3,14 @@ package dk.itu.eyedroid;
 import java.io.IOException;
 
 import dk.itu.eyedroid.io.InputNetStreamingProtocol;
+import dk.itu.eyedroid.io.OutputNetTCPProtocol;
 import dk.itu.spcl.jlpf.core.Filter;
 import dk.itu.spcl.jlpf.core.ProcessingCore;
 import dk.itu.spcl.jlpf.io.IOController;
-import dk.itu.spcl.jlpf.io.IOProtocolReader;
 import dk.itu.spcl.jlpf.io.IOProtocolWriter;
 import dk.itu.spcl.jlpf.io.IORWDefaultImpl;
+import dk.itu.spcl.jlpf.io.InputReader;
+import dk.itu.spcl.jlpf.io.OutputWriter;
 import android.app.Fragment;
 import android.graphics.Bitmap;
 import android.os.Bundle;
@@ -19,6 +21,7 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 
 public class TestFragment extends Fragment {
+	
 	final String URL = "http://217.197.157.7:7070/axis-cgi/mjpg/video.cgi?resolution=320x240";
 
 	private View mRootView;
@@ -28,41 +31,35 @@ public class TestFragment extends Fragment {
 	private IOController ioController;
 
 	@Override
-	public View onCreateView(LayoutInflater inflater, ViewGroup container,
-			Bundle savedInstanceState) {
+	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
-		mRootView = inflater.inflate(R.layout.streaming_layout, container,
-				false);
+		mRootView = inflater.inflate(R.layout.streaming_layout, container,false);
 		mImageView = (ImageView) mRootView.findViewById(R.id.mjpeg_view);
 
-		InputNetStreamingProtocol inProtocol = new InputNetStreamingProtocol(
-				URL);
-		IORWDefaultImpl readerWriter = new IORWDefaultImpl(inProtocol,
-				new TestWriter());
+		InputNetStreamingProtocol inProtocol = new InputNetStreamingProtocol(URL);
+		OutputNetTCPProtocol outProtocol = new OutputNetTCPProtocol(6000);
+		//TestWriter outProtocol = new TestWriter();
+		IORWDefaultImpl io_rw = new IORWDefaultImpl(inProtocol, outProtocol);
 
 		core = new ProcessingCore();
 		core.addFilter(new TestFilter());
 		core.addFilter(new TestFilter());
 
-		ioController = new AndroidIOController(core, readerWriter, readerWriter);
+		ioController = new AndroidIOController(core, io_rw, io_rw);
 
 		return mRootView;
 	}
 
 	@Override
 	public void onResume() {
-		// TODO Auto-generated method stub
 		super.onResume();
 		core.start(1);
 		ioController.start();
-
 	}
 
 	@Override
 	public void onPause() {
-		// TODO Auto-generated method stub
 		super.onPause();
-
 		core.stop();
 		ioController.stop();
 	}
@@ -72,37 +69,26 @@ public class TestFragment extends Fragment {
 		private ReadingThread readerThread;
 		private WritingThread writerThread;
 
-		public AndroidIOController(ProcessingCore core,
-				dk.itu.spcl.jlpf.io.InputReader reader,
-				dk.itu.spcl.jlpf.io.OutputWriter writer) {
+		public AndroidIOController(ProcessingCore core, InputReader reader, OutputWriter writer) {
 			super(core, reader, writer);
-			// TODO Auto-generated constructor stub
 		}
 
 		@Override
 		public void init() {
-			// TODO Auto-generated method stub
-
+			readerThread = new ReadingThread();
+			writerThread = new WritingThread();
 		}
 
 		@Override
 		protected void onExecute() {
-
-			readerThread = new ReadingThread();
-			writerThread = new WritingThread();
-
 			readerThread.start();
 			writerThread.start();
-
 		}
 
 		@Override
 		protected void onStop() {
-			// TODO Auto-generated method stub
-
 			readerThread.stopThread();
 			writerThread.stopThread();
-
 		}
 
 		public class ReadingThread extends Thread {
@@ -114,17 +100,14 @@ public class TestFragment extends Fragment {
 				((IORWDefaultImpl) InputReader).initReader();
 				while (!isStopped) {
 					try {
-						Log.i("------------------",
-								"reader thread is fucking running");
+						Log.i("---------------","reader thread is fucking running");
 						AndroidIOController.this.read();
 					} catch (IOException e) {
 						((IORWDefaultImpl) InputReader).cleanup();
-						// TODO Auto-generated catch block
 						e.printStackTrace();
 						break;
 					}
 				}
-
 			}
 
 			public void stopThread() {
@@ -142,14 +125,14 @@ public class TestFragment extends Fragment {
 
 			@Override
 			public void run() {
+				((IORWDefaultImpl) OutputWriter).initWriter();
 				while (!isStopped) {
 					try {
-						Log.i("------------------",
-								"writer thread is fucking running");
+						Log.i("---------------","writer thread is fucking running");
 						AndroidIOController.this.write();
 					} catch (IOException e) {
 						((IORWDefaultImpl) OutputWriter).cleanup();
-						// TODO Auto-generated catch block
+						((IORWDefaultImpl) OutputWriter).initWriter();
 						e.printStackTrace();
 						break;
 					}
@@ -171,36 +154,25 @@ public class TestFragment extends Fragment {
 		}
 
 		@Override
-		public void write(dk.itu.spcl.jlpf.common.Bundle arg0)
-				throws IOException {
+		public void write(dk.itu.spcl.jlpf.common.Bundle arg0) throws IOException {
+			Log.i("---------------", "Writer is active");
 
-			Log.i("-----------------------------", "Writer is active");
-
-			final Bitmap bitmap = (Bitmap) arg0
-					.get(InputNetStreamingProtocol.INPUT_BITMAP);
+			final Bitmap bitmap = (Bitmap) arg0.get(InputNetStreamingProtocol.INPUT_BITMAP);
 			TestFragment.this.getActivity().runOnUiThread(new Runnable() {
 				@Override
 				public void run() {
 					mImageView.setImageBitmap(bitmap);
-
 				}
 			});
-
 			arg0 = null;
-
 		}
 	}
 
 	public class TestFilter extends Filter {
-
 		@Override
-		protected dk.itu.spcl.jlpf.common.Bundle execute(
-				dk.itu.spcl.jlpf.common.Bundle arg0) {
-			// TODO Auto-generated method stub
-			Log.i("-------------------------", "in filter");
+		protected dk.itu.spcl.jlpf.common.Bundle execute( dk.itu.spcl.jlpf.common.Bundle arg0) {
+			Log.i("---------------", "in filter");
 			return arg0;
 		}
-
 	}
-
 }
