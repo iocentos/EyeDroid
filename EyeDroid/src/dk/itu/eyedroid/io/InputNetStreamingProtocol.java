4c0e92b2dc87ca83634a5a6ce00e938953bbbb10
Ioannis Sintos <sintosioannis@outlook.com>
2014-10-18 17:00:17 +0200

    Added io package and inputProtocol

---
+++ EyeDroid/src/dk/itu/eyedroid/io/InputNetStreamingProtocol.java
@@ -0,0 +1,46 @@
+package dk.itu.eyedroid.io;
+
+import java.io.IOException;
+
+import android.graphics.Bitmap;
+import dk.itu.spcl.jlpf.common.Bundle;
+import dk.itu.spcl.jlpf.io.IOProtocolReader;
+
+public class InputNetStreamingProtocol implements IOProtocolReader{
+	
+	
+	public static final String INPUT_BITMAP = "initial_bitmap";
+	
+	private String mUrl;
+	private MjpegInputStream mInputStream;
+	
+	public InputNetStreamingProtocol(String url) {
+		mUrl = url;
+	}
+
+	@Override
+	public void cleanup() {
+		try {
+			mInputStream.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	@Override
+	public void init() {
+		mInputStream = MjpegInputStream.read(mUrl);
+	}
+
+	@Override
+	public Bundle read() throws IOException{
+		Bundle bundle = new Bundle();
+		Bitmap bitmap = mInputStream.readMjpegFrame();
+		
+		bundle.put(INPUT_BITMAP, bitmap);
+		
+		return bundle;
+	}
+
+}
