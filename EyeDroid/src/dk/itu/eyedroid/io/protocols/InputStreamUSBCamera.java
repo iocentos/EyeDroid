a36536c0293c9a521356f4c5925bb864175be741
Ioannis Sintos <sintosioannis@outlook.com>
2014-10-22 20:05:56 +0200

    Fixed crash when closing usb camera.

--- EyeDroid/src/dk/itu/eyedroid/io/protocols/InputStreamUSBCamera.java
+++ EyeDroid/src/dk/itu/eyedroid/io/protocols/InputStreamUSBCamera.java
@@ -15,7 +15,7 @@ import dk.itu.spcl.jlpf.io.IOProtocolReader;
 public class InputStreamUSBCamera implements IOProtocolReader {
 
 	private static final boolean DEBUG = true;
-	private static final String TAG = "WebCam";
+	private static final String TAG = "UsbWebCam";
 	protected Context context;
 	Thread mainLoop = null;
 	private Bitmap bmp = null;
@@ -27,7 +27,7 @@ public class InputStreamUSBCamera implements IOProtocolReader {
 	// In some omap devices, system uses /dev/video[0-3],
 	// so users must use /dev/video[4-].
 	// In such a case, try cameraId=0 and cameraBase=4
-	private int cameraId = 3;
+	private int cameraId = 0;
 	private int cameraBase = 0;
 
 	// This definition also exists in ImageProc.h.
@@ -53,32 +53,30 @@ public class InputStreamUSBCamera implements IOProtocolReader {
 
 	public native void pixeltobmp(Bitmap bitmap);
 
-
-	
-	public InputStreamUSBCamera( Context context , int cameraId) {
+	public InputStreamUSBCamera(Context context, int cameraId) {
 		this.context = context;
 		this.cameraId = cameraId;
 	}
 
 	@Override
 	public void cleanup() {
+		Log.i(TAG, "Cleaning up usb camera with device id " + cameraId);
 		if (DEBUG)
 			Log.d(TAG, "surfaceDestroyed");
-		if (cameraExists) {
-			shouldStop = true;
-			while (shouldStop) {
-				try {
-					Thread.sleep(100); // wait for thread stopping
-				} catch (Exception e) {
-				}
-			}
-		}
 		stopCamera();
 
 	}
 
 	@Override
 	public void init() throws IOException {
+
+		Process sh = Runtime.getRuntime().exec(
+				new String[] {
+						"su",
+						"-c",
+						"/system/bin/sh -c \"chmod 666 /dev/video" + cameraId
+								+ "\"" });
+		Log.i(TAG, "Initiating usb camera with device id " + cameraId);
 		if (DEBUG)
 			Log.d(TAG, "surfaceCreated");
 		if (bmp == null) {
@@ -88,27 +86,35 @@ public class InputStreamUSBCamera implements IOProtocolReader {
 		// /dev/videox (x=cameraId + cameraBase) is used
 		int ret = prepareCameraWithBase(cameraId, cameraBase);
 
-		if (ret != -1)
+		if (ret != -1) {
 			cameraExists = true;
+			Log.i(TAG, "Usb camera with device id " + cameraId + " is open");
+		}
+
+		else {
+			Log.i(TAG, "Usb camera with device id " + cameraId
+					+ "Could not open");
+			throw new IOException("Could not open usb camera with device id "
+					+ cameraId);
+		}
 	}
 
 	@Override
 	public Bundle read() throws IOException {
-//		 obtaining a camera image (pixel data are stored in an array in JNI).
+		// obtaining a camera image (pixel data are stored in an array in JNI).
 		if (cameraExists) {
 			processCamera();
 			// camera image to bmp
 			pixeltobmp(bmp);
 
 			Bundle bundle = new Bundle();
+			Log.i(TAG, "Usb camera got new frame " + cameraId
+					+ "Could not open");
 			bundle.put(InputNetStreamingProtocol.INPUT_BITMAP, bmp);
 			return bundle;
 		}
-		
-		Bundle b = new Bundle();
-		b.put(InputNetStreamingProtocol.INPUT_BITMAP, BitmapFactory.decodeResource(context.getResources(), R.drawable.ic_launcher));
 
-		return b;
+		throw new IOException("Usb camera is not open. Could not read frame");
 	}
 
 }
